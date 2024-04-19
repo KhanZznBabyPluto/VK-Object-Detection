d1ce2c75a198d0521097845fed5f23f2681eb6b7
KhanZznBabyPluto <khangeldinansar@gmail.com>
2024-04-19 22:53:46 +0300

    RMSE ~ 2.75

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from rmse import RMSE
+# from rmse import RMSE
 import tensorflow as tf
 from data_preprocessing import data_processing, image_processing
 
@@ -11,11 +11,14 @@ train_data, test_data = data_processing(train_data_path)
 train_images = image_processing(train_data[:, 0]) / 255.0
 test_images = image_processing(test_data[:, 0]) / 255.0
 
+train_images = np.expand_dims(train_images, axis=-1)
+test_images = np.expand_dims(test_images, axis=-1)
+
 train_labels = train_data[:, 1].astype(int)
 test_labels = test_data[:, 1].astype(int)
 
-train_images = np.expand_dims(train_images, axis=-1)
-test_images = np.expand_dims(test_images, axis=-1)
+# train_types = tf.keras.utils.to_categorical(train_data[:, 2].astype(int))
+# test_types = tf.keras.utils.to_categorical(test_data[:, 2].astype(int))
 
 model = tf.keras.models.Sequential([
     tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
@@ -28,11 +31,12 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(1)
 ])
 
-model.compile(optimizer='adam',
+model.compile(optimizer='adamax',
               loss='mean_squared_error',
-              metrics=['mae', RMSE()])
+              metrics=['root_mean_squared_error'])
 
-print(train_labels.shape)
-print(train_images.shape)
+# print(train_labels.shape)
+# print(train_images.shape)
 
+# history = model.fit(train_images, train_labels, train_types, epochs=10, validation_data=(test_images, test_labels, test_types))
 history = model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
