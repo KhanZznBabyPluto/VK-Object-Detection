9d18b47db714e1801aa0b62ed31d557d3c3f8ae6
KhanZznBabyPluto <khangeldinansar@gmail.com>
2024-04-19 15:18:01 +0300

    Not optimal yet

--- contours.py
+++ contours.py
@@ -1,13 +1,15 @@
 import cv2
 
-def find_contours(image_path):
+def find_contours(image_path, median_size=5):
     image = cv2.imread(image_path)
 
     shifted = cv2.pyrMeanShiftFiltering(image, 3, 90)
 
     gray = cv2.cvtColor(shifted, cv2.COLOR_BGR2GRAY)
 
-    ret, threshold = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_TRIANGLE)
+    # ret, threshold = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_TRIANGLE)\
+
+    threshold = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 
     contours, hierarchy = cv2.findContours(threshold, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
 
@@ -22,5 +24,5 @@ def find_contours(image_path):
 
     return contours
 
-# path = 'train_data/2.png'
-# find_contours(path)
+path = 'train_data/1.png'
+find_contours(path)
---
+++ square_counter.py
@@ -0,0 +1,26 @@
+import cv2
+from contours import find_contours
+from shape_detector import ShapeDetector
+
+def square_counter(image):
+
+    contours = find_contours(image)
+
+    shape_detector = ShapeDetector()
+
+    square_counter = 0
+
+    if contours[-1][0][0][0] == 0 and contours[-1][0][0][1] == 0 and contours[-1][-1][0][1] == 0:
+        contours = contours[0:-1]
+
+    for contour in contours:
+        if shape_detector.detect(contour) == "square":
+            square_counter +=1
+
+    print(square_counter)
+
+    return
+
+path = 'train_data/1.png'
+square_counter(path)
+
